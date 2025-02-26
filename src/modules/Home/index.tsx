48603d7ed993375667c092a64bb2cfe12d39bad7
Huy Vo <huyvo@var-meta.com>
2025-02-26 22:49:15 +0700

    chore: test images and icons

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -10,10 +10,10 @@ const HomePage = () => {
   return (
     <VStack spacing={64} className="mb-24 space-y-2 md:space-y-12">
       home page Hot Match
-      <div className="relative w-20 h-20">
+      <div className="relative h-20 w-20">
         <Image src="/images/game1.png" alt="" fill className="object-cover" />
       </div>
-      <Icons.games className="w-20 h-20 text-black" />
+      <Icons.games className="h-20 w-20 text-black" />
     </VStack>
   );
 };
